411c879ff1fcb75b69ffcc775685610715490b5c
ioamra <iom.monchau@gmail.com>
2022-10-16 10:34:57 +0200

    useless datatable module

--- public/js/matchs.js
+++ public/js/matchs.js
@@ -98,22 +98,6 @@ function loop(data) {
     }
 }
 
-//* module datatable for sorting
-jQuery.extend(jQuery.fn.dataTableExt.oSort, {
-    "formatted-num-pre": function (a) {
-        a = a === "-" || a === "" ? 0 : a.replace(/[^\d\-\.]/g, "");
-        return parseFloat(a);
-    },
-
-    "formatted-num-asc": function (a, b) {
-        return a - b;
-    },
-
-    "formatted-num-desc": function (a, b) {
-        return b - a;
-    },
-});
-
 function veiwsInfoMatchs(data) {
     let dataGames = "";
     let dataGameOne = "";
